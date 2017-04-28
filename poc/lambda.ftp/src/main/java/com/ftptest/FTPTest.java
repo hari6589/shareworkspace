991b34014553df39f89219841fc773844cd28abc
Aravindhan Jayakumar <aravindhan.jayakumar@csscorp.com>
2017-04-28 19:47:13 +0530

    Lambda FTP file as InputStream

--- poc/lambda.ftp/src/main/java/com/ftptest/FTPTest.java
+++ poc/lambda.ftp/src/main/java/com/ftptest/FTPTest.java
@@ -1,22 +1,15 @@
 package com.ftptest;
 
-
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.jcraft.jsch.Channel;
 import com.jcraft.jsch.ChannelSftp;
 import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
 
 public class FTPTest implements RequestHandler<Object, Object> {
@@ -27,11 +20,9 @@ public class FTPTest implements RequestHandler<Object, Object> {
 	}
 	
 	public void sftpTest() {
-		//JSch.setLogger(new JSCHLogger()); // More on this below
 		JSch jsch = new JSch();
 		Session session = null;
 		ChannelSftp channel = null;
-		FileInputStream localFileStream = null;
 		
 		String YOUR_KEY_FILE_NAME = "src/main/resources/key.txt";
 		String YOUR_SSH_SERVER_USER_NAME = "oracle";
@@ -40,15 +31,15 @@ public class FTPTest implements RequestHandler<Object, Object> {
 		String YOUR_SSH_PASSWORD = "";
 		String YOUR_SSH_SERVER_PASSWORD = "";
 		String YOUR_SSH_SERVER_FILE_DIRECTORY = "/opt/home/oracle/test";
-		String YOUR_LOCAL_FILE_TO_COPY = "D:/test.txt";
+		//String YOUR_LOCAL_FILE_TO_COPY = "D:/test.txt";
 		String YOUR_REMOTE_FILE_NAME = "SFTP_TEST.txt";
 		String YOUR_KEY_FILE_PASS_PHRASE="puttydsa";
-		
+		System.out.println("Starts Execution..");
 		try
 		{
 			//Use key authentication if it is set, else use password auth
 			if (YOUR_KEY_FILE_NAME != null && YOUR_KEY_FILE_NAME != "") {
-				
+				System.out.println("Trying to add identity with Key file!");
 			    Path path = Paths.get(YOUR_KEY_FILE_NAME);
 			    byte[] prvKey = Files.readAllBytes(path);
 			    
@@ -62,6 +53,7 @@ public class FTPTest implements RequestHandler<Object, Object> {
 				
 				session = jsch.getSession(YOUR_SSH_SERVER_USER_NAME, YOUR_SSH_SERVER_NAME, YOUR_SSH_SERVER_PORT);
 			} else if (YOUR_SSH_PASSWORD != null && YOUR_SSH_PASSWORD != "") {
+				System.out.println("Trying to add identity with Password!");
 				session = jsch.getSession(YOUR_SSH_SERVER_USER_NAME, YOUR_SSH_SERVER_NAME, YOUR_SSH_SERVER_PORT);
 				session.setPassword(YOUR_SSH_SERVER_PASSWORD);
 			}
@@ -80,14 +72,9 @@ public class FTPTest implements RequestHandler<Object, Object> {
 				System.out.println("Directory Changed!");  
 			}
 			
-			File localFile = new File (YOUR_LOCAL_FILE_TO_COPY);
-			if (localFile.exists()) {
-				localFileStream = new FileInputStream(localFile);
-				channel.put(localFileStream, YOUR_REMOTE_FILE_NAME);
-				System.out.println("File Moved!");
-			} else {
-				System.out.println("Local file not found " + localFile.getAbsolutePath());
-			}
+			ByteArrayInputStream stream = new ByteArrayInputStream("test data".getBytes(StandardCharsets.UTF_8));
+			channel.put(stream, YOUR_REMOTE_FILE_NAME);
+			System.out.println("File Moved!");
 			
 		} catch(Exception e) {
 			System.out.println("Exception : " + e.getMessage());
