a0abdbbed30b7849e347eb1f9fde60cd5df23330
Aravindhan Jayakumar <aravindhan.jayakumar@csscorp.com>
2017-06-21 18:25:36 +0530

    Jenkins Auto Build and AWS Deploy Test

--- poc/hello/src/main/java/hello/Hello.java
+++ poc/hello/src/main/java/hello/Hello.java
@@ -5,8 +5,9 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 public class Hello implements RequestHandler<Object, Object> {
 
-	public Object handleRequest(Object input, Context context){ {
+	public Object handleRequest(Object input, Context context) {
 		return "Hello 123";
 	}
 
 }
+	
