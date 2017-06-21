04ca1f409fc68a1f25b122e81a616224eadd7372
Aravindhan Jayakumar <aravindhan.jayakumar@csscorp.com>
2017-06-21 15:52:18 +0530

    Jenkins Hello Test

---
+++ poc/hello/src/main/java/hello/Hello.java
@@ -0,0 +1,12 @@
+package hello;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class Hello implements RequestHandler<Object, Object> {
+
+	public Object handleRequest(Object input, Context context) {
+		return "Hello";
+	}
+
+}
